1aca36eb1551ed08096bef20d48c6b6c5a9d1204
Scott <sbtcoles@yahoo.com>
2010-12-01 00:14:50 -0600

    First onegit status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '97ddacd35b0d2d4a1b00e4a7e75e9ffa7549d5cc402beade68062ed38630c442c6b3c7088ffe13dcdd185857bb2d7013567b70ff6434a23fa9cad0df87136df3'
